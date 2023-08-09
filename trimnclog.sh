594d75d7c6286a2ef584a0cb3860fcc756b4002b
Skyrider <79767107+skyrid3r698@users.noreply.github.com>
2023-08-09 20:50:00 +0200

    Create trimnclog.sh

---
+++ trimnclog.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#variables
+x="360" #number of days to work through
+logdir="/home/sky"
+logfile="nextcloud.log"
+workdir="/tmp"
+cp $logdir/$logfile $workdir/
+#trim everything before time of each line
+sed -i 's/^.*time":"/'/ $workdir/$logfile
+#trim everything except last x days of log entries
+#read each day and append it to a file
+if test -f "$workdir/tempx.txt"; then
+    rm $workdir/tempx.txt
+fi
+while [ $x -gt 0 ];
+do
+XOLD=`(date +"%Y-%m-%d"  --date="$x days ago")`
+let x--
+cat $workdir/$logfile | grep "$XOLD" >> $workdir/tempx.txt
+done
+mv $workdir/tempx.txt $workdir/$logfile
+
+########failed logins############
+#delete every line without "Login failed"
+sed '/Login failed/!d' $workdir/$logfile > $workdir/failedlogins.txt
+#delete everything between "user": and message including "user":
+sed -i 's/"user":.*message/"message'/ $workdir/failedlogins.txt
+#delete everything after version of each line
+sed -i 's/"."version.*//g' $workdir/failedlogins.txt
+#delete every instance of '
+sed -i "s/'//g" $workdir/failedlogins.txt
+mv $workdir/failedlogins.txt ./
+
+###########unique ips###########
+#delete every line without "remoteAddr"
+sed '/remoteAddr/!d' $workdir/$logfile > $workdir/uniqueip.txt
+#remove every line with ajax in it
+sed -in '/ajax/d' $workdir/uniqueip.txt
+#trim everything before remoteAddr":" of each line
+sed -i 's/^.*remoteAddr":"/'/ $workdir/uniqueip.txt
+#delete everything after Remote IP of each line
+sed -i 's/","user.*//g' $workdir/uniqueip.txt
+#delete empty lines
+sed -i '/^$/d' $workdir/uniqueip.txt
+# sortieren
+sort $workdir/uniqueip.txt | uniq -c | sort -nr > $workdir/temp.txt
+mv $workdir/temp.txt uniqueip.txt
+
+###########unique names###########
+#delete every line without "remoteAddr"
+sed '/remoteAddr/!d' $workdir/$logfile > $workdir/uniquenames.txt
+#remove every line with ajax in it
+sed -in '/ajax/d' $workdir/uniquenames.txt
+#trim everything before user":" of each line
+sed -i 's/^.*user":"/'/ $workdir/uniquenames.txt
+#delete everything after Remote IP of each line
+sed -i 's/","app.*//g' $workdir/uniquenames.txt
+#delete empty lines
+sed -i '/^$/d' $workdir/uniquenames.txt
+# sortieren
+sort $workdir/uniquenames.txt | uniq -c | sort -nr > $workdir/temp2.txt
+mv $workdir/temp2.txt uniquenames.txt
