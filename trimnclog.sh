af179f37da7c61fc9c296e05cb520d0cea14b49d
Skyrider <79767107+skyrid3r698@users.noreply.github.com>
2023-10-24 19:44:39 +0200

    clear up messages

--- trimnclog.sh
+++ trimnclog.sh
@@ -26,7 +26,7 @@ echo set temporary file as new $workdir/$logfile
 mv $workdir/tempx.txt $workdir/$logfile
 
 ########failed logins############
-echo creating $workdir/failedlogins.txt
+echo creating $workdir/failedlogins.txt and moving it to current directory
 #delete every line without "Login failed"
 sed '/Login failed/!d' $workdir/$logfile > $workdir/failedlogins.txt
 #delete everything between "user": and message including "user":
@@ -38,7 +38,7 @@ sed -i "s/'//g" $workdir/failedlogins.txt
 mv $workdir/failedlogins.txt ./
 
 ###########unique ips###########
-echo creating $workdir/uniqueip.txt
+echo creating $workdir/uniqueip.txt and moving it to current directory
 #delete every line without "remoteAddr"
 sed '/remoteAddr/!d' $workdir/$logfile > $workdir/uniqueip.txt
 #remove every line with ajax in it
@@ -54,7 +54,7 @@ sort $workdir/uniqueip.txt | uniq -c | sort -nr > $workdir/temp.txt
 mv $workdir/temp.txt uniqueip.txt
 
 ###########unique names###########
-echo creating $workdir/uniquenames.txt
+echo creating $workdir/uniquenames.txt and moving it to current directory
 #delete every line without "remoteAddr"
 sed '/remoteAddr/!d' $workdir/$logfile > $workdir/uniquenames.txt
 #remove every line with ajax in it
